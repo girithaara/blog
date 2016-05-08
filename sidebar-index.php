02731d06b432de81a7e8f2071c7a4cf302e5d9e1
girithaara <girithaara@gmail.com>
2016-05-08 20:26:13 +0800

    first commit

---
+++ sidebar-index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package Bushwick
+ */
+?>
+
+<header id="masthead" class="site-header" role="banner">
+	<img src="http://snidgetapp.com/assets/logowhite.png" class="text-center logo">
+	<div class="site-branding">
+
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+	</div>
+</header><!-- #masthead -->
